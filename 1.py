168155d221a8624a8a83949618086091b0237225
SaraSun1223 <jp2019213204@qmul.ac.uk>
2023-11-10 22:46:35 +0800

    11.10

---
+++ 1.py
@@ -0,0 +1,41 @@
+# 1. 两数之和
+# 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+#
+# 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+#
+# 你可以按任意顺序返回答案。
+# 1 暴力枚举法
+# 思路及算法
+#
+# 最容易想到的方法是枚举数组中的每一个数 x，寻找数组中是否存在 target - x。
+#
+# 当我们使用遍历整个数组的方式寻找 target - x 时，需要注意到每一个位于 x 之前的元素都已经和 x 匹配过，因此不需要再进行匹配。而每一个元素不能被使用两次，所以我们只需要在 x 后面的元素中寻找 target - x。
+#
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+
+
+# 2 哈希表
+# 思路及算法
+#
+# 注意到方法一的时间复杂度较高的原因是寻找 target - x 的时间复杂度过高。因此，我们需要一种更优秀的方法，能够快速寻找数组中是否存在目标元素。如果存在，我们需要找出它的索引。
+#
+# 使用哈希表，可以将寻找 target - x 的时间复杂度降低到从 O(N)O(N)O(N) 降低到 O(1)O(1)O(1)。
+#
+# 这样我们创建一个哈希表，对于每一个 x，我们首先查询哈希表中是否存在 target - x，然后将 x 插入到哈希表中，即可保证不会让 x 和自己匹配。
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            # Python enumerate() 函数
+            # Python 内置函数 Python 内置函数
+            # enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标，一般用在 for 循环当中。
+            if target - num in hashtable:
+                return [hashtable[target-num],i]
+            hashtable[nums[i]]=i
+        return []
